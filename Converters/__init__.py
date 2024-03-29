8c9278b0b0f553858a44ef383667d77fbf31460d
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-03-29 14:22:33 -0500

    Add files via upload

---
+++ Converters/__init__.py
@@ -0,0 +1,29 @@
+__Program__     = "Converters.__init__"    
+__programer__   = "Themadhood Pequot"
+__Date__        = "7/26/2023"
+__version__     = "0.0.1"
+__update__      = ""
+__info__        = ""
+
+try:
+    from . import _ToPy as ToPy
+except:
+    import _ToPy as ToPy
+
+
+
+"""except Exception as e:
+    if self._Error:
+        raise
+    Error.UploadError([__Program__,__version__,"class","def",
+                                f"mesage",e],"Functions")"""
+
+
+
+
+
+
+
+
+
+
